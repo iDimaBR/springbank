4b52921b7ce1a30784e7f7a3170fd9520311390d
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 17:20:55 -0300

    feat: criado Controlador de Usuários

---
+++ src/main/java/com/github/idimabr/springbank/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.github.idimabr.springbank.controllers;
+
+import com.github.idimabr.springbank.dtos.UserDTO;
+import com.github.idimabr.springbank.entities.User;
+import com.github.idimabr.springbank.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        final List<User> users = this.userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
