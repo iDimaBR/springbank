4be9a3d1da2cd3399263aa17d56b391bcf2a11a1
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 18:41:35 -0300

    feat: Adicionado novo tratamento para método não suportado.

--- src/main/java/com/github/idimabr/springbank/infra/ControllerExceptionHandler.java
+++ src/main/java/com/github/idimabr/springbank/infra/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.github.idimabr.springbank.dtos.ExceptionDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -28,5 +29,11 @@ public class ControllerExceptionHandler {
     public ResponseEntity threatGeneralException(Exception exception){
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
         return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity threatRequestMethod(HttpRequestMethodNotSupportedException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Método de requisição não aceito.", "404");
+        return ResponseEntity.badRequest().body(exceptionDTO);
     }
 }
