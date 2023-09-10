32e1042a83e9a1c69499cbacefa37c55c030b39c
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 17:18:35 -0300

    fix: Removido construtor sem argumentos.

--- src/main/java/com/github/idimabr/springbank/entities/Transaction.java
+++ src/main/java/com/github/idimabr/springbank/entities/Transaction.java
@@ -1,10 +1,7 @@
 package com.github.idimabr.springbank.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.time.LocalDateTime;
 @Table(name = "transactions")
 @Getter @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode
 public class Transaction {
 
