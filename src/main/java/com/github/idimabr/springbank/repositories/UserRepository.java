0dbb050a5709d33c830f3651760778745fdb77c0
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 18:02:09 -0300

    feat: Adicionado novamente o método findUserByEmail para melhora do sistema.

--- src/main/java/com/github/idimabr/springbank/repositories/UserRepository.java
+++ src/main/java/com/github/idimabr/springbank/repositories/UserRepository.java
@@ -10,5 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 
     Optional<User> findUserByDocument(String document);
     Optional<User> findUserById(Long id);
+    Optional<User> findUserByEmail(String email);
 
 }
