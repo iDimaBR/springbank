aec54eff5b84c4701552891e61cf912352a8b3e0
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 18:03:18 -0300

    fix: Adicionado construtor sem argumentos.

--- src/main/java/com/github/idimabr/springbank/entities/User.java
+++ src/main/java/com/github/idimabr/springbank/entities/User.java
@@ -3,10 +3,7 @@ package com.github.idimabr.springbank.entities;
 
 import com.github.idimabr.springbank.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -15,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class User {
 
