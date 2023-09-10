2f877066da58929df3a8e3829618d9f682d2d2eb
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 14:38:37 -0300

    feat: initial config

---
+++ src/main/java/com/github/idimabr/springbank/SpringbankApplication.java
@@ -0,0 +1,13 @@
+package com.github.idimabr.springbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbankApplication.class, args);
+	}
+
+}
