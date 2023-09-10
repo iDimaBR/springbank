12414ea0f634bca117843015f3acb1b3ad3e7f47
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 18:02:54 -0300

    fix: Corrigido problema ao enviar dados de formulário para criação de usuário.

--- src/main/java/com/github/idimabr/springbank/controllers/UserController.java
+++ src/main/java/com/github/idimabr/springbank/controllers/UserController.java
@@ -6,9 +6,7 @@ import com.github.idimabr.springbank.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController()
