b636e45939049ff3f7d3b4d203b5400eef280359
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 17:23:07 -0300

    feat: Criado o serviço de usuários

---
+++ src/main/java/com/github/idimabr/springbank/services/UserService.java
@@ -0,0 +1,41 @@
+package com.github.idimabr.springbank.services;
+
+import com.github.idimabr.springbank.dtos.UserDTO;
+import com.github.idimabr.springbank.entities.User;
+import com.github.idimabr.springbank.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw  new Exception("Saldo insuficiente.");
+        }
+    }
+
+    public User findUserById(Long id){
+        return this.repository.findUserById(id).orElseThrow();
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
+    }
+}
