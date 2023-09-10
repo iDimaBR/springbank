f0fa2c2ca64397f730e78cd48dfeeaa1e763ef40
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 18:05:07 -0300

    feat: Adicionado controle de exceções para tratamento de erros

---
+++ src/main/java/com/github/idimabr/springbank/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.github.idimabr.springbank.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/github/idimabr/springbank/infra/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.github.idimabr.springbank.infra;
+
+import com.github.idimabr.springbank.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.OptionalDataException;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário já cadastrado no sistema.", "400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threatNotFound(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
