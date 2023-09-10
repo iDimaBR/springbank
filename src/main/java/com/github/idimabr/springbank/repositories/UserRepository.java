1c64d034718c1a39b168be4a0836bcba1a058c06
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 17:22:48 -0300

    BREAKING CHANGE: Removido o método FindUserByEmail do repositório.

--- src/main/java/com/github/idimabr/springbank/repositories/UserRepository.java
+++ src/main/java/com/github/idimabr/springbank/repositories/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long>{
 
     Optional<User> findUserByDocument(String document);
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserById(Long id);
 
 }
