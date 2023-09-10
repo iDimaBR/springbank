c1d8a613cad069abc4872f311f9929ba34c1f0fe
Raphael Alexei <raphaeeelbr@gmail.com>
2023-09-10 14:41:24 -0300

    feat: create UserRepository

---
+++ src/main/java/com/github/idimabr/springbank/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.github.idimabr.springbank.repositories;
+
+import com.github.idimabr.springbank.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserByEmail(String email);
+
+}
